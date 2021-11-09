e7f91c4f1dc0d56b24057415b9ed2cdf9093529b
Lukasz Sobel <lukasz.sobel@hotmail.com>
2021-11-09 17:30:53 +0100

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="container">
+      <header>
+         <h1>Fraternia</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
