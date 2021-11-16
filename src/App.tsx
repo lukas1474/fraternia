9da213d03e2397d7f5de4bcae62489f7ced040b4
Lukasz Sobel <lukasz.sobel@hotmail.com>
2021-11-16 09:54:20 +0100

    Prepare work in progress page with logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import "./App.css";
 const App = () => {
   return (
     <div className="container">
       <header>
-         <h1>Fraternia</h1>
+         <h1>Fraternia Zakonna</h1>
+         <img src="/images/LOGO.svg" alt="fraternia" />
+         <h2>Strona w budownie</h2>
       </header>
     </div>
   );
